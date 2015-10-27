465748c2b67f906d65ff070d158a7e7cb12753e0
石川達也 <stone.guitar.777@gmail.com>
2015-10-28 01:19:07 +0900

    セレニウムの実験環境作成

---
+++ Project/Test/Sample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using System.IO;
+
+namespace Test
+{
+    [TestClass]
+    public class Sample
+    {
+        FirefoxDriver _driver;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _driver = new FirefoxDriver();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _driver.Dispose();
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            var path = Path.GetFullPath("../../../Target/Index.html");
+            var url = "file:///" + path;
+            _driver.Url = url;
+        }
+    }
+}
